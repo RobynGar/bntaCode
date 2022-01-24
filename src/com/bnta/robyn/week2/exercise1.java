72f4754d5fda2251473bd135625349084f3cb21e
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-01-24 17:51:41 +0000

    week 2 exercises

--- src/com/bnta/robyn/week2/exercise1.java
+++ src/com/bnta/robyn/week2/exercise1.java
@@ -3,16 +3,17 @@ package com.bnta.robyn.week2;
 public class exercise1 {
 
     public static void main(String[] args) {
-        reverse("test a only is this");
+        System.out.println(reverse("tset"));
     }
 
     static String reverse(String text) {
-        String[] array = text.split(" ");
         String output = "";
+        // above works as a placeholder for the reversed string
         for (int i = text.length() - 1; i >= 0; i--) {
-            System.out.print(text.charAt(i));
+            output += text.charAt(i);
+            //above adds the character to the last reversed character
         }
-    return "reverse";
+    return "test";
     }
 }
 
--- src/com/bnta/robyn/week2/exercise2.java
+++ src/com/bnta/robyn/week2/exercise2.java
@@ -1,4 +1,24 @@
 package com.bnta.robyn.week2;
 
 public class exercise2 {
-}
+        /*
+          Given the following String input
+          String input = "   b ri   ght net   work  "
+          Write a program that transforms input into: Bright Network
+        */
+       public static void main(String[] args) {
+        String input = "   b ri   ght net   work  ";
+        input = input.trim().replaceAll("\\s+", "");
+
+        input = input.replace('b', 'B');
+        input = input.replace('n', 'N');
+
+           System.out.println(input);
+
+            }
+
+        }
+
+
+
+
