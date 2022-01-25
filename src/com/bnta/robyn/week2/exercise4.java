a778e5a18bd01dd3dc3eb540dfe3dbb15cf61da9
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-01-25 15:02:03 +0000

    week 2 exercises

--- src/com/bnta/robyn/week2/exercise4.java
+++ src/com/bnta/robyn/week2/exercise4.java
@@ -1,5 +1,7 @@
 package com.bnta.robyn.week2;
 
+import java.util.Scanner;
+
 public class exercise4 {
      /*
       Write a program that takes an input number from the
@@ -7,5 +9,19 @@ public class exercise4 {
       Create a method to check if number is prime
       then use against the input
     */
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("Enter a number");
 
+     }
+     public static boolean prime(int number){
+         if (number <=1){
+             return false;
+         }
+         for (int i = 2; i < number; i++){
+             if (number % 1 ==0){
+                 return true;
+             }
+         }
+     }
 }
