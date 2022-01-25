226095ae4a621edbf55340cb779ff446d06f3333
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-01-25 14:30:02 +0000

    week 2 exercises

---
+++ src/com/bnta/robyn/week2/exercise4.java
@@ -0,0 +1,11 @@
+package com.bnta.robyn.week2;
+
+public class exercise4 {
+     /*
+      Write a program that takes an input number from the
+      console and prints if number is prime
+      Create a method to check if number is prime
+      then use against the input
+    */
+
+}
