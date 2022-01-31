575fd914e294a4da5962f07bcef8b735206e3093
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-01-31 13:51:53 +0000

    enums practice

---
+++ src/com/bnta/robyn/week2/tshirtSize.java
@@ -0,0 +1,19 @@
+package com.bnta.robyn.week2;
+
+public enum tshirtSize {
+    // create enum to represent t-shirt sizes
+        SMALL,
+        MEDIUM,
+        LARGE,
+        XLARGE,
+        XXLARGE
+
+
+}
+
+
+
+
+
+
+
