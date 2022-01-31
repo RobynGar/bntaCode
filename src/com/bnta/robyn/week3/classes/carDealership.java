19baadf7b230f6a67f0df0a8771c4a59f191b3ff
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-01-31 16:54:05 +0000

    car classes

---
+++ src/com/bnta/robyn/week3/classes/carDealership.java
@@ -0,0 +1,4 @@
+package com.bnta.robyn.week3.classes;
+
+public class carDealership {
+}
---
+++ src/com/bnta/robyn/week3/classes/engineType.java
@@ -0,0 +1,7 @@
+package com.bnta.robyn.week3.classes;
+
+public enum engineType {
+    PETROL,
+    DIESEL,
+    ELECTRIC
+}
