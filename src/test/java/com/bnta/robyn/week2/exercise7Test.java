7b37c2b717c17926bb66f2feb348ce2aecc7562c
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-02-06 15:59:34 +0000

    tests for longest string

--- src/test/java/com/bnta/robyn/week2/exercise7Test.java
+++ src/test/java/com/bnta/robyn/week2/exercise7Test.java
@@ -2,17 +2,50 @@ package com.bnta.robyn.week2;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class exercise7Test {
 
     @Test
-    void longest() {
+    void shouldGiveYouTheLongestStringInArray() {
         //give
-
+    String[] input = {"long", "longer", "longest"};
         //then
-
+    String actual = exercise7.longest(input);
         //when
-
+    String expected = "longest";
+    assertThat(actual).isEqualTo(expected);
     }
+    @Test
+    void whenDuplicateShouldOnlyGiveYouLongestWordOnce() {
+        //give
+        String[] input = {"long", "longer", "longest", "longest"};
+        //then
+        String actual = exercise7.longest(input);
+        //when
+        String expected = "longest";
+        assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    void whenSameLengthButDifferentWordShouldGiveBoth() {
+        //give
+        String[] input = {"long", "longer", "longest", "tongest"};
+        //then
+        String actual = exercise7.longest(input);
+        //when
+        String expected = "longest, tongest";
+        assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    void whenGivenNullShouldGiveEmptyString() {
+        //give
+        String[] input = null;
+        //then
+        String actual = exercise7.longest(input);
+        //when
+        String expected = "";
+        assertThat(actual).isEqualTo(expected);
+    }
+
 }
